33818366e64341e28b7dbb85669683b888eaec2c
Abhay Rana <capt.n3m0@gmail.com>
2015-03-15 17:22:36 +0530

    Allow user to copy the rtsp path, instead of relying on xclip

--- stream-audio.sh
+++ stream-audio.sh
@@ -3,9 +3,8 @@ set -o nounset
 
 pulseaudio_stream='pulse://alsa_output.pci-0000_00_1b.0.analog-stereo'
 
-echo "rtsp://me.captnemo.in:8554/"|xclip
-echo "Copied RTSP url to clipboard. Paste on device"
+echo "Copy the following URL and paste in on device: rtsp://me.captnemo.in:8554/"
+read -t 5 -p "Hit ENTER or wait ten seconds"
 echo "Starting VLC"
-sleep 2
 
 nvlc "pulse://" ":sout=#transcode{vcodec=none,acodec=mpga,ab=128,channels=2,samplerate=44100}:rtp{sdp=rtsp://:8554/}"

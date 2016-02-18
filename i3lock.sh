ce297e9189f68e1037c97ebfad9fc7b4b75c211b
Abhay Rana <capt.n3m0@gmail.com>
2016-02-18 23:33:08 +0530

    i3lock update

--- i3lock.sh
+++ i3lock.sh
@@ -3,32 +3,30 @@ cd ~/projects/scripts
 xset -b #disable scrot beep
 #!/bin/bash
 
-# Dependencies: imagemagick, i3lock-color-git, scrot
-
-IMAGE=$(mktemp).png
-#TEXT="Type password to unlock"
-
-#VALUE="60" #brightness value to compare to
-scrot $IMAGE
-
-# pixelate: -scale 10% -scale 1000%
-# if [ "$COLOR" -gt "$VALUE" ]; then #white background image and black text
-#     convert $IMAGE -level 0%,100%,0.6 -filter Gaussian -resize 20% -define filter:sigma=1.5 -resize 500% - | composite lockscreen.png - -compose over $IMAGE
-#     PARAM='--textcolor=00000000 --insidecolor=0000001c --ringcolor=0000003e --linecolor=00000000 --keyhlcolor=ffffff80 --ringvercolor=ffffff00 --insidevercolor=ffffff1c --ringwrongcolor=ffffff55 --insidewrongcolor=ffffff1c'
-# else #black
-#     convert $IMAGE -level 0%,100%,0.6 -filter Gaussian -resize 20% -define filter:sigma=1.5 -resize 500% - | composite lockscreen.png - -compose over $IMAGE
-#     PARAM='--textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c'
-# fi
-
-convert $IMAGE -level 0%,100%,0.6 -filter Gaussian -resize 20% -define filter:sigma=1.5 -resize 500% - | composite lockscreen.png - -compose over $IMAGE
+# Now that the rest of the images have been generated
+# We can just call it using that
 PARAM='--textcolor=00000000 --insidecolor=0000001c --ringcolor=0000003e --linecolor=00000000 --keyhlcolor=ffffff80 --ringvercolor=ffffff00 --insidevercolor=ffffff1c --ringwrongcolor=ffffff55 --insidewrongcolor=ffffff1c'
+IMAGE_COMPOSITE="composite_office_lockscreen.png"
 
-# try to use a forked version of i3lock with prepared parameters
-i3lock $PARAM -i $IMAGE > /dev/null 2>&1
+i3lock $PARAM -i $IMAGE_COMPOSITE
+exit
 
-if [ $? -ne 0 ]; then
-    # We have failed, lets get back to stock one
-    i3lock -i $IMAGE
-fi
+# Dependencies: imagemagick, i3lock-color-git, scrot
+function generate_composite_image()
+{
+	#IMAGE=$(mktemp).png
+	IMAGE_BASE="/home/nemo/Pictures/office_screen_lock.png"
+	IMAGE_TEMP="/tmp/tmp_lockscreen.png"
+	cp $IMAGE_BASE $IMAGE_TEMP
+	#scrot $IMAGE
 
-rm $IMAGE
+	convert $IMAGE_TEMP -level 0%,100%,0.6 -filter Gaussian -resize 20% -define filter:sigma=1.5 -resize 500% - | composite lockscreen_office.png - -compose over $IMAGE_TEMP
+
+	# try to use a forked version of i3lock with prepared parameters
+	i3lock $PARAM -i $IMAGE_TEMP > /dev/null 2>&1
+
+	if [ $? -ne 0 ]; then
+	    # We have failed, lets get back to stock one
+	    i3lock -i $IMAGE
+	fi
+}

5b578f46dceb850cf34cffc900b1f13e3a2612bc
Abhay Rana <me@captnemo.in>
2016-07-18 18:12:25 +0530

    Unlocks desktop using yubikey

---
+++ yubi-insert.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+YUBIKEY_SERIAL=`cat /home/nemo/.yubikey-serial.txt`
+DEVICE_SERIAL=`ykinfo -s | cut -d ' ' -f 2`
+LOGFILE="/home/nemo/logs/auth.log"
+
+# We only unlock the device if the key matches
+if [[ "$YUBIKEY_SERIAL"="$DEVICE_SERIAL" ]]; then
+	echo "`date` unlocked using yubkikey" >> $LOGFILE
+	killall i3lock
+fi

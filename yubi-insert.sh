ea4dfa09ce8b40be6377667eb23dac835d6c368e
Abhay Rana <me@captnemo.in>
2016-12-21 17:18:37 +0530

    [auth] Improves yubikey handling

--- yubi-insert.sh
+++ yubi-insert.sh
@@ -4,7 +4,7 @@ DEVICE_SERIAL=`ykinfo -s | cut -d ' ' -f 2`
 LOGFILE="/home/nemo/logs/auth.log"
 
 # We only unlock the device if the key matches
-if [[ "$YUBIKEY_SERIAL"="$DEVICE_SERIAL" ]]; then
+if [[ "$YUBIKEY_SERIAL" -eq "$DEVICE_SERIAL" ]]; then
 	echo "`date` unlocked using yubkikey" >> $LOGFILE
 	killall i3lock
 fi
--- yubilock.sh
+++ yubilock.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-/bin/su nemo -c "DISPLAY=:0 xautolock -locknow"
+LOGFILE="/home/nemo/logs/auth.log"
+
+/bin/su nemo -c "DISPLAY=:0 /home/nemo/projects/scripts/i3lock.sh" >> $LOGFILE
+echo "`date` locked using Yubikey $?" >> $LOGFILE

b5f08f8c43ff0fb5ae8c77fcb05e221adf6483d1
Abhay Rana <me@captnemo.in>
2017-01-30 16:23:09 +0530

    Minor indent fix

--- screen-darken.sh
+++ screen-darken.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Remember to disable redshift before running this script. Otherwise, flux will 
 #reset the brightness level every second or so.
- xrandr --output eDP1 --brightness 0.3
+xrandr --output eDP1 --brightness 0.3

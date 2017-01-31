5bf493bbda9a9c2eab5f406cbba981f6a0818dde
Abhay Rana <me@captnemo.in>
2017-01-31 19:56:47 +0530

    Addded ExecStop to the i3lock service

--- yubi-insert.sh
+++ yubi-insert.sh
@@ -3,8 +3,10 @@ YUBIKEY_SERIAL=`cat /home/nemo/.yubikey-serial.txt`
 DEVICE_SERIAL=`ykinfo -s | cut -d ' ' -f 2`
 LOGFILE="/home/nemo/logs/auth.log"
 
+export DISPLAY=:0
+
 # We only unlock the device if the key matches
 if [[ "$YUBIKEY_SERIAL" -eq "$DEVICE_SERIAL" ]]; then
 	echo "`date` unlocked using yubkikey" >> $LOGFILE
-	killall i3lock
+	/usr/bin/systemctl stop i3lock.service
 fi

2ce5d405aa3a37ff2d3e39983811131398a3089d
Nemo <me@captnemo.in>
2018-07-24 18:52:48 +0530

    Adds i3lock-fancy

--- cbz-convertor.sh
+++ cbz-convertor.sh
@@ -20,7 +20,7 @@ done < <(find . -mindepth 3 -maxdepth 3 -type d)
 # Sync it as well
 for dir in *; do
     cd $dir
-    rsync  --archive --include '*.cbz'  --recursive * nemo@192.168.1.111:/mnt/xwing/media/EBooks/Comics/ --verbose --exclude '*.jpg' --exclude '*.png' --exclude '.sync' --prune-empty-dirs 
+    rsync  --archive --include '*.cbz'  --recursive * nemo@10.8.0.14:/mnt/xwing/media/EBooks/Comics/ --verbose --exclude '*.jpg' --exclude '*.png' --exclude '.sync' --prune-empty-dirs 
     cd ..
 done
 

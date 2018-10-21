2ddd9fd62d80c0ea3f87eff230cd21965708b003
Nemo <me@captnemo.in>
2018-10-21 18:10:17 +0530

    Housekeeping improvements

--- lowbatterycron.sh
+++ lowbatterycron.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# Add the following to your crontab to get this working
+# DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+# XAUTHORITY=/home/nemo/.Xauthority
 BATTINFO=`acpi -b`
 BATTERY=/sys/class/power_supply/BAT0
 REM=`grep "POWER_SUPPLY_CHARGE_NOW" $BATTERY/uevent | awk -F= '{ print $2 }'`
 FULL=`grep "POWER_SUPPLY_CHARGE_FULL_DESIGN" $BATTERY/uevent | awk -F= '{ print $2 }'`
 PERCENT=`echo $(( $REM * 100 / $FULL ))`
 
-if [[ `echo $BATTINFO | grep Discharging` && "$PERCENT" -lt "10" ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send "low battery" "$BATTINFO"
+echo "Battery is at $PERCENT%"
+
+if [[ `echo $BATTINFO | grep Discharging` && "$PERCENT" -lt "15" ]] ; then
+	echo "Not Charging, so alerting"
+    DISPLAY=:0.0 /usr/bin/notify-send --urgency=critical "Battery Running Low" "$BATTINFO"
 fi
--- signcerts.sh
+++ signcerts.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export CF_DNS_SERVERS='1.1.1.1 9.9.9.9'
 
 # Based on https://gist.github.com/benkulbertis/fff10759c2391b6618dd/
-export CF_KEY=`gkeyring -n CF_KEY --output secret`
+export CF_KEY=$(pass show CloudFlare/CF_KEY)
 export CF_EMAIL="capt.n3m0@gmail.com"
 
 dehydrated --cron

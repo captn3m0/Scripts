8cfeea8ea9b3044c2939cb87878b5f9e2a19e29c
Nemo <me@captnemo.in>
2020-04-08 03:36:09 +0530

    Put titles on each file, because the big file title won't work well

--- signcerts.sh
+++ signcerts.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 export CF_DNS_SERVERS='1.1.1.1 9.9.9.9'
+export CF_SETTLE_TIME='30'
 
 # Based on https://gist.github.com/benkulbertis/fff10759c2391b6618dd/
 export CF_KEY=$(pass show Keys/CF_KEY)
